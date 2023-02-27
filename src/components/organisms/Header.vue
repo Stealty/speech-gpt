cb96d836643d8e962b58e6deac74a815cfb92480
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-27 15:39:25 -0300

    feat: Header component

---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+});
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="header__title">{{ title }}</h1>
+    <p class="header__description">
+      {{ description }}
+    </p>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  color: var(--va-quarternary);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 3rem 0;
+
+  &__title {
+    font-size: 3rem;
+  }
+  &__description {
+    text-align: center;
+  }
+}
+</style>
