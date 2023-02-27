1441562f61cb69d220e7eb8b3c3b1bdbc00864a7
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-27 15:38:46 -0300

    feat: chatCard component

---
+++ src/components/molecules/ChatCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  log: {
+    question: String,
+    answer: String,
+  },
+});
+</script>
+
+<template>
+  <va-card class="card">
+    <va-card-title class="card__user">You:</va-card-title>
+    <va-card-content class="card__content">
+      {{ log.question }}
+    </va-card-content>
+  </va-card>
+  <va-card class="card">
+    <va-card-title class="card__user">SpeechGPT:</va-card-title>
+    <va-card-content class="card__content">
+      {{ log.answer }}
+    </va-card-content>
+  </va-card>
+</template>
+
+<style scoped lang="scss">
+.card {
+  overflow: hidden;
+  min-width: 227px;
+  max-width: 500px;
+  color: var(--va-primary) !important;
+
+  &__user {
+    background-color: var(--va-quarternary);
+  }
+
+  &__content {
+    background-color: var(--va-quarternary);
+    &::first-letter {
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
