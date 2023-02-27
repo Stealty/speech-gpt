c68d78b3d4fe49472e91cffb877c57403b8524c2
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-27 15:38:15 -0300

    feat: component Button

---
+++ src/components/atoms/Button.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  click: Function,
+  name: String | HTMLElement,
+});
+</script>
+
+<template>
+  <va-button class="button" @click="click">{{ name }}</va-button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  color: var(--va-primary) !important;
+
+  &::before {
+    background-color: var(--va-quarternary);
+  }
+}
+</style>
