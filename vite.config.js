00bb99e1fb9de5c3d73b5ad7ce948cd017d97bd1
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-28 23:11:19 -0300

    feat: add alias

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+      "@stores": fileURLToPath(new URL("./src/stores", import.meta.url)),
+      "@use": fileURLToPath(new URL("./src/use", import.meta.url)),
+      "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@icons": fileURLToPath(
+        new URL("./src/components/icons", import.meta.url)
+      ),
+      "@ions": fileURLToPath(new URL("./src/components/ions", import.meta.url)),
+      "@atoms": fileURLToPath(
+        new URL("./src/components/atoms", import.meta.url)
+      ),
+      "@molecules": fileURLToPath(
+        new URL("./src/components/molecules", import.meta.url)
+      ),
+      "@organisms": fileURLToPath(
+        new URL("./src/components/organisms", import.meta.url)
+      ),
+      "@templates": fileURLToPath(
+        new URL("./src/components/templates", import.meta.url)
+      ),
+    },
+  },
+});
