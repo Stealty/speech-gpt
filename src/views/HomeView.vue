cd018dd4d9cdd03cda1a8ff5f8b84328bc6049fe
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-28 23:04:08 -0300

    feat: clear HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,14 @@
-<!-- eslint-disable no-undef -->
 <script setup>
-import { useSpeechRecognition } from "@vueuse/core";
-import { useSpeechSynthesis } from "@vueuse/core";
-import { ref } from "vue";
 import Mic from "../components/icons/Mic.vue";
-import Header from "../components/organisms/Header.vue";
 import Button from "../components/atoms/Button.vue";
+import Header from "../components/organisms/Header.vue";
 import ChatLog from "../components/organisms/ChatLog.vue";
-import ThemeSwitcher from "../components/molecules/ThemeSwitcher.vue";
+import { useChatStore } from "../stores/chatStore";
+import useRecognition from "@use/useRecognition";
 
-const { result, start, recognition, stop, isListening } = useSpeechRecognition({
-  lang: "en-US",
-  continuous: false,
-});
+const { start, stop, isListening } = useRecognition();
 
-const { utterance, speak } = useSpeechSynthesis();
-
-const chatLog = ref([]);
-
-recognition.onend = () => {
-  const voices = window.speechSynthesis.getVoices();
-  if (result.value.length > 0) {
-    fetch("https://speech-gpt-server.vercel.app/api", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: result.value,
-        apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-      }),
-    })
-      .then((response) => response)
-      .then((data) => data.json())
-      .then(async (data) => {
-        utterance.value.text = data.bot;
-        utterance.value.lang = "en-US";
-        utterance.value.voice = voices[1];
-        chatLog.value.unshift({
-          time: new Date(),
-          question: result.value,
-          answer: data.bot,
-        });
-        speak();
-        result.value = "";
-      })
-      .catch((error) => console.error(error));
-  } else {
-    alert("Say something...");
-  }
-
-  stop();
-};
+const { chatLog } = useChatStore();
 </script>
 
 <template>
@@ -59,10 +16,10 @@ recognition.onend = () => {
     title="SpeechGPT"
     description=" What do you want? Let's talk about something!"
   />
-  <ThemeSwitcher />
   <main class="home__main">
     <ChatLog :chat-log="chatLog" />
     <Mic :is-listening="isListening" />
+    <button :on-click="useRecognition">asdasd</button>
     <div class="home__buttons">
       <Button name="Press to talk" :click="start" />
       <Button name="Press to stop talking" :click="stop" />
